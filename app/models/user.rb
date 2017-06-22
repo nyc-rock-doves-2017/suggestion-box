8c27961a56d53503109723d525709e7dcfe50510
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-06-22 11:01:50 -0400

    adding bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ActiveRecord::Base
     end
   end
 
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
